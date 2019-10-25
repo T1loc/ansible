e8e9bbb284cec8ffb870e4171919f75a407f05bf
LucasBoisserie <lucas.boisserie@gmail.com>
2019-10-25 11:19:23 +0200

    clean comment

--- lib/ansible/modules/cloud/misc/helm_cli.py
+++ lib/ansible/modules/cloud/misc/helm_cli.py
@@ -248,9 +248,6 @@ except ImportError:
 
 from ansible.module_utils.basic import AnsibleModule
 
-# TODO:
-#   * rollback (need to parse release['Chart'] in get_release_status which of type chart_name-semver)
-
 module = None
 is_helm_2 = True
 
